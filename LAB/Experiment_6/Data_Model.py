13cf46b7ec6f40616ea7d3e58cab5133aa6d47be
Sravani Koyyala <112328653+Jk7s@users.noreply.github.com>
2026-09-23 23:54:30 +0530

    Add dataclass implementation for Student data

--- LAB/Experiment_6/Data_Model.py
+++ LAB/Experiment_6/Data_Model.py
@@ -1,1 +1,30 @@
+from dataclasses import dataclass
 
+
+# Traditional class
+class Student:
+    def __init__(self, name: str, id: int, age: int) -> None:
+        self.name = name
+        self.id = id
+        self.age = age
+
+    def show(self) -> None:
+        print(self.name, self.id, self.age)
+
+
+# Dataclass
+@dataclass
+class StudentData:
+    name: str
+    id: int
+    age: int
+
+
+s1 = Student("Suma", 101, 22)
+s2 = StudentData("Tabu", 102, 23)
+
+print("Traditional Class:")
+s1.show()
+
+print("Dataclass:")
+print(s2)
