6b0ceff2c03cdb0cfba0cbfe0745d5e027e49788
Sravani Koyyala <112328653+Jk7s@users.noreply.github.com>
2026-09-23 19:54:14 +0530

    Update Banking_System.py

--- LAB/Experiment_5/Banking_System.py
+++ LAB/Experiment_5/Banking_System.py
@@ -1,1 +1,48 @@
+from abc import ABC, abstractmethod
 
+
+class BankAccount(ABC):
+
+    def __init__(self, name: str, balance: int) -> None:
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount: int) -> None:
+        self.balance += amount
+
+    @abstractmethod
+    def withdraw(self, amount: int) -> None:
+        pass
+
+    def show(self) -> None:
+        print("Name:", self.name)
+        print("Balance:", self.balance)
+
+
+class SavingsAccount(BankAccount):
+
+    def withdraw(self, amount: int) -> None:
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Insufficient balance")
+
+
+class CurrentAccount(BankAccount):
+
+    def withdraw(self, amount: int) -> None:
+        self.balance -= amount
+
+
+s = SavingsAccount("Sravani", 5000)
+
+s.deposit(1000)
+s.withdraw(2000)
+s.show()
+
+
+c = CurrentAccount("Ravi", 5000)
+
+c.deposit(1000)
+c.withdraw(3000)
+c.show()
